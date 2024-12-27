e14c20dbd9ae2d5196025441fbe9acd221926b80
Vaishnavi2282 <vaishnavi.jadhav0802@gmail.com>
2024-12-27 21:33:29 +0530

    Update and rename schemas to schemas.py

---
+++ app/schemas.py
@@ -0,0 +1,21 @@
+from pydantic import BaseModel
+from typing import Optional
+
+class UserCreate(BaseModel):
+    username: str
+    email: str
+    password: str
+
+class UserLogin(BaseModel):
+    username: str
+    password: str
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+class UserInProfile(BaseModel):
+    username: str
+    full_name: Optional[str] = None
+    email: str
+
