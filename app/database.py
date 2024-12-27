7505a0a7648445b6604c7bd82eede7a187c6acaa
Vaishnavi2282 <vaishnavi.jadhav0802@gmail.com>
2024-12-27 21:32:29 +0530

    Update database.py

--- app/database.py
+++ app/database.py
@@ -1,4 +1,11 @@
 from pymongo import MongoClient
+import os
 
-client = MongoClient("mongodb://localhost:27017/")
-db = client["todolist"]
+# Database URL from environment or config file
+MONGO_URI = os.getenv("MONGO_URI", "mongodb://localhost:27017")
+client = MongoClient(MONGO_URI)
+db = client.todo_db
+
+# Collections
+users_collection = db["users"]
+todos_collection = db["todos"]
